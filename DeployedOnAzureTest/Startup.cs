d197b4a1b40e9965d06575b261a6904cd8c9cfcb
Abhishek Malwankar <Abhishek Malwankar>
2016-09-05 14:11:08 -0600

    Initial Commit for web deployed asp .net web app on azure

---
+++ DeployedOnAzureTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DeployedOnAzureTest.Startup))]
+namespace DeployedOnAzureTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
